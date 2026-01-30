194d27d5494db023dc26d329f82cece27447725f
artsiom <mazarchuk.artem@yandex.ru>
2026-01-30 10:35:07 +0300

    Spring_client

---
+++ src/main/java/com/example/client/repository/ClientRepository.java
@@ -0,0 +1,7 @@
+package com.example.client.repository;
+
+import com.example.client.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
