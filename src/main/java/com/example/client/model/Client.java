f025971dc5ea50ef494c491bdfcd76b24d1b81ec
Artsiom Mazarchuk <mazarchuk.artem@yandex.ru>
2026-02-01 13:48:34 +0300

    insert main

---
+++ src/main/java/com/example/client/model/Client.java
@@ -0,0 +1,36 @@
+package com.example.client.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
+    public Client() {}
+
+    public Client(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/example/client/service/DefaultClientService.java
@@ -0,0 +1,40 @@
+package com.example.client.service;
+
+import com.example.client.model.Client;
+import com.example.client.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DefaultClientService {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    public Client createClient(String name) {
+        return clientRepository.save(new Client(name));
+    }
+    // Получить все записи
+    public List<Client> getAllClients() {
+        return clientRepository.findAll();
+    }
+
+    public Client updateClient(Long id, String newName) {
+        Client client = clientRepository.findById(id).orElseThrow();
+        client.setName(newName);
+        return clientRepository.save(client);
+    }
+
+    public void deleteClientById(Long id) {
+        clientRepository.deleteById(id);
+    }
+
+    public Optional<Client> getClientById(Long id)  {
+        return clientRepository.findById(id);
+    }
+
+
+}
